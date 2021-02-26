b9972443a2221a85b468bf747bf2e67415635404
Karandashev Konstantin <kkarandashev@l43.vsc4.prov>
2021-02-26 17:58:53 +0100

    Recompiler script no longer tries to delete qml when it is not installed.

--- tests/recompile_qml.sh
+++ tests/recompile_qml.sh
@@ -2,7 +2,10 @@
 
 MYDIR=$(pwd)
 package_name=qml
-rm -Rf "$(pip show --verbose qml | awk '{if ($1=="Location:") print $2}')/$package_name"
+if [ -z "$(pip show $package_name 2>&1 >/dev/null)" ]
+then
+    rm -Rf "$(pip show --verbose $package_name | awk '{if ($1=="Location:") print $2}')/$package_name"
+fi
 cd ..
 rm -f __pycache__/*
 rm -Rf build/*
