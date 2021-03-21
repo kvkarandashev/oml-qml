6047b9b202355e5342d33abc0ad4d5d7ccc52c10
kvkarandashev <kvkarandashev@gmail.com>
2021-03-21 19:13:03 +0100

    Added a new module for more convenient scripting related to the QM7b-T dataset.

---
+++ python_script_modules/qm7b_t_format_specs.py
@@ -0,0 +1,75 @@
+from qml.oml_compound import OML_compound
+import subprocess, glob
+
+class QuantityNotAvailableError(Exception):
+    pass
+
+
+from urllib.parse import quote_plus
+
+from qml.oml_compound import OML_compound
+
+def_float_format='{:.8E}'
+
+au_to_kcalmol_mult=627.50960803
+
+def potential_energy(xyz_name, calc_type="HF", basis="sto-3g", dft_xc='lda,vwn', dft_nlc='', pyscf_calc_params=None):
+    oml_comp=OML_compound(xyz = xyz_name, mats_savefile = xyz_name, calc_type=calc_type, basis=basis, dft_xc=dft_xc, dft_nlc=dft_nlc, pyscf_calc_params=pyscf_calc_params)
+    oml_comp.run_calcs()
+    return oml_comp.e_tot*au_to_kcalmol_mult
+
+class Quantity:
+    def __init__(self, quant_name):
+        self.name=quant_name
+    def extract_xyz(self, filename):
+        file=open(filename, 'r')
+        lines=file.readlines()
+        output=None
+        for l in lines:
+            lsplit=l.split()
+            if len(lsplit)>1:
+                if lsplit[0] == self.name:
+                    output=float(lsplit[2])
+                    break
+        file.close()
+        if output is None:
+            raise QuantityNotAvailableError
+        else:
+            return output
+    def OML_calc_quant(self, xyz_name, calc_type="HF", basis="sto-3g", pyscf_calc_params=None):
+        return potential_energy(xyz_name, calc_type=calc_type, basis=basis, pyscf_calc_params=pyscf_calc_params)
+
+class Quantity_diff:
+    def __init__(self, quant_name1, quant_name2):
+        self.quant1=Quantity(quant_name1)
+        self.quant2=Quantity(quant_name2)
+    def extract_xyz(self, filename):
+        return self.quant2.extract_xyz(filename)-self.quant1.extract_xyz(filename)
+    # Perhaps should be improved.
+    def OML_calc_quant(self, xyz_name, calc_type="HF", basis="sto-3g"):
+        return self.quant2.OML_calc_quant(xyz_name, calc_type=calc_type, basis=basis)-self.quant1.OML_calc_quant(xyz_name, calc_type=calc_type, basis=basis)
+        
+
+def file_to_2Dlist(filename):
+    inp=open(filename, 'r')
+    return [l.split(',') for l in inp.read().split('\n')]
+
+def create_xyz_files(QM7bT_dir, output_dir):
+    val_lines = file_to_2Dlist(QM7bT_dir+"/values.csv")
+    value_names=val_lines[0]
+    for val_row in val_lines[1:]:
+        xyz_id=val_row[0]
+        new_xyz_name=output_dir+'/ext_'+xyz_id+'.xyz'
+        possible_xyzs=glob.glob(QM7bT_dir+"/molecules/"+xyz_id+'__*.xyz')
+        if len(possible_xyzs)>1:
+            print("xyz id duplicated")
+            quit()
+        subprocess.run(["cp", '-f', possible_xyzs[0], new_xyz_name])
+        cur_xyz=open(new_xyz_name, "a+")
+        for val, val_name in zip(val_row, value_names):
+            if val_name != '':
+                cur_xyz.write(val_name+" : "+str(val)+'\n')
+        cur_xyz.close()
+    val_csvfile.close()
+    entry_csvfile.close()
+            
