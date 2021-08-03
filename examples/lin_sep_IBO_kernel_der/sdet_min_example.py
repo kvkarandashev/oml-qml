b61f6b5e8e276cb17a53f52954845f974f769f04
kvkarandashev <kvkarandashev@gmail.com>
2021-08-03 17:43:47 +0200

    Revised minimal example for hyperparameter optimization with single Slater determinants.

--- examples/lin_sep_IBO_kernel_der/sdet_min_example.py
+++ examples/lin_sep_IBO_kernel_der/sdet_min_example.py
@@ -10,8 +10,8 @@ import os, random, qml
 import numpy as np
 from qml import OML_compound_list_from_xyzs
 from qml.oml_representations import OML_rep_params
-from qml.oml_kernels import lin_sep_IBO_sym_kernel, lin_sep_IBO_kernel
-from qml.hyperparameter_optimization import  min_sep_IBO_MSE_random_walk
+from qml.oml_kernels import gauss_sep_IBO_sym_kernel, gauss_sep_IBO_kernel
+from qml.hyperparameter_optimization import min_sep_IBO_random_walk_optimization
 from qml.utils import dump2pkl
 
 def_float_format='{:.8E}'
@@ -25,8 +25,11 @@ max_angular_momentum=1
 # Replace with path to QM9 directory
 QM7bT_dir=os.environ["DATA"]+"/QM7bT_reformatted"
 
-train_num=800
+train_num=500
 check_num=2000
+
+use_Gauss=True
+keep_init_lambda=False
 
 delta_learning_params=Delta_learning_parameters(use_delta_learning=True)
 
@@ -45,23 +48,30 @@ def get_quants_comps(xyz_list, quantity, dl_params, oml_representation_parameter
     quant_vals=import_quantity_array(xyz_list, quantity, dl_params)
     comps=OML_compound_list_from_xyzs(xyz_list, calc_type="HF", basis=basis)
     comps.generate_orb_reps(oml_representation_parameters)
-    return comps, quant_vals
+    return comps, np.array(quant_vals)
 
 quant=Quantity(quant_name)
 training_comps, training_quants=get_quants_comps(xyz_list[:train_num], quant, delta_learning_params, oml_representation_parameters)
 
-optimized_hyperparams=min_sep_IBO_MSE_random_walk(training_comps, training_quants, init_lambda_guess=1e-6, training_set_ratio=0.5, initial_guess_sigma_rescale=1.0, step_magnitude=2.0, num_iters=40, negligible_below_default=-9.21034, negligible_below_lambda=-16.1181, exclusion_permanent=True)
+optimized_hyperparams=min_sep_IBO_random_walk_optimization(training_comps, training_quants, init_lambda=1e-6, use_Gauss=use_Gauss, max_stagnating_iterations=16, num_kfolds=32,
+                                    hyperparam_red_type="ang_mom_classified", randomized_iterator_kwargs={"keep_init_lambda" : keep_init_lambda, "default_step_magnitude" : 0.25}, iter_dump_name_add="test_min",
+                                    rep_params=oml_representation_parameters)
+
+
 
 inv_sq_width_params=optimized_hyperparams["inv_sq_width_params"]
 lambda_val=optimized_hyperparams["lambda_val"]
 
-K_train=lin_sep_IBO_sym_kernel(training_comps, inv_sq_width_params)
+print("Finalized parameters:", inv_sq_width_params)
+print("Finalized lambda:", lambda_val)
+
+K_train=gauss_sep_IBO_sym_kernel(training_comps, inv_sq_width_params)
 K_train[np.diag_indices_from(K_train)]+=lambda_val
 alphas=np_cho_solve_wcheck(K_train, training_quants, eigh_rcond=1e-9)
 del(K_train)
 
 check_comps, check_quants=get_quants_comps(xyz_list[-check_num:], quant, delta_learning_params, oml_representation_parameters)
-K_check=lin_sep_IBO_kernel(check_comps, training_comps, inv_sq_width_params)
+K_check=gauss_sep_IBO_kernel(check_comps, training_comps, inv_sq_width_params)
 predicted_quants=np.dot(K_check, alphas)
 MAE=np.mean(np.abs(predicted_quants-check_quants))
 print("Quantity: ", quant_name, ", MAE:", MAE)
